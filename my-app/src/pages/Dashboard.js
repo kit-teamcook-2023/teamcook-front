d12e55ef7e73bf1dbd6b03cd8a6104566c5f8568
choijungwo <93969236+choijungwo@users.noreply.github.com>
2023-06-11 19:44:45 +0900

    가스비 소비차트 수정중(잘못올림)
    그래프 완성 가스비 받아오기 함수 제작중

--- my-app/src/pages/Dashboard.js
+++ my-app/src/pages/Dashboard.js
@@ -1,85 +1,98 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWonSign, faPercent } from '@fortawesome/free-solid-svg-icons';
+import Chart from 'react-apexcharts';
+import axios from 'axios';
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gasData: {
+        series: [
+          {
+            name: '이번달 가스비',
+            data: [20000],
+          },
+          {
+            name: '저번달 가스비',
+            data: [10000],
+          },
+          {
+            name: '이번 달 예상 가스비',
+            data: [30000],
+          },
+        ],
+        options: {
+          chart: {
+            type: 'bar',
+            height: 400,
+          },
+          plotOptions: {
+            bar: {
+              horizontal: true,
+            },
+          },
+          xaxis: {
+            categories: ['가스비'],
+          },
+        },
+      },
+    };
+  }
+
+  componentDidMount() {
+    this.patchGasData();
+  }
+
+  patchGasData = () => {
+    let TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuaWNrbmFtZSI6InRlc3QyIiwidWlkIjoiMTAyODkwMTcyOTA0IiwiZXhwIjoxNjg2NDYxMTQxfQ.hVuuTc1GQQw8-Dtp3Vm6LkKjPFcI3TG--63Hisx1pvI';
+
+    axios.get('http://34.215.66.235:8000/user-fee', {
+      headers: {
+        'Authorization': 'Bearer ' + TOKEN
+      }
+    })
+      .then(res => {
+        console.log(res.status);
+        console.log(res.data);
+
+        const updatedGasData = {
+          ...this.state.gasData,
+          series: res.data,
+        };
+
+        this.setState({ gasData: updatedGasData });
+      })
+      .catch(err => {
+        console.log(err.response.status);
+      });
+  }
 
   render() {
-    
-    document.title="대시 보드"
-    
+    document.title = '대시 보드';
+
     return (
-        <div id="page-wrapper">
-          <div className="row">
-            <div className="col-lg-12">
-              <h1 className="page-header">실시간 가스 요금</h1>
-            </div>
-            {/*  /.col-lg-12  --> */}
-          </div>
-          {/*  /.row  --> */}
-          <div className="row">
-            <div className="col-lg-3 col-md-6">
-              <div className="panel panel-primary">
-                <div className="panel-heading">
-                  <div className="row">
-                    <div className="col-xs-3">
-                      <FontAwesomeIcon icon={faWonSign} size="5x"/>
-                    </div>
-                    <div className="col-xs-9 text-right">
-                      <div className="huge">30,000</div>
-                      <div>이번달 가스비</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-3 col-md-6">
-              <div className="panel panel-green">
-                <div className="panel-heading">
-                  <div className="row">
-                    <div className="col-xs-3">
-                      <FontAwesomeIcon icon={faWonSign} size="5x"/>
-                    </div>
-                    <div className="col-xs-9 text-right">
-                      <div className="huge">10,000</div>
-                      <div>저번달 가스비</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-3 col-md-6">
-              <div className="panel panel-yellow">
-                <div className="panel-heading">
-                  <div className="row">
-                    <div className="col-xs-3">
-                      <FontAwesomeIcon icon={faPercent} size="5x"/>
-                    </div>
-                    <div className="col-xs-9 text-right">
-                      <div className="huge">30</div>
-                      <div>/ 설정값</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-3 col-md-6">
-              <div className="panel panel-red">
-                <div className="panel-heading">
-                  <div className="row">
-                    <div className="col-xs-3">
-                      <FontAwesomeIcon icon={faWonSign} size="5x"/>
-                    </div>
-                    <div className="col-xs-9 text-right">
-                      <div className="huge">50,000</div>
-                      <div>이번 달 예상 가스비</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+      <div id="page-wrapper">
+        <div className="row">
+          <div className="col-lg-12">
+            <h1 className="page-header">나의 가스 소비 차트</h1>
           </div>
         </div>
+        <div>
+          <button onClick={this.patchGasData}>가스비 확인</button>
+        </div>
+        <div className="row">
+          <div className="col-lg-6">
+            <Chart
+              options={this.state.gasData.options}
+              series={this.state.gasData.series}
+              type="bar"
+              height={200}
+              width={1500}
+            />
+          </div>
+        </div>
+      </div>
     );
   }
 }
