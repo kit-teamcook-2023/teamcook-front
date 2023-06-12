ff51c90b93e014cda8fe1962be1d8cfa26b8cabc
PromotezCitizen <gkstkdgus821@gmail.com>
2023-06-13 00:28:06 +0900

    commentList를 main과 동일하게 수정

--- my-app/src/components/post/CommentList.js
+++ my-app/src/components/post/CommentList.js
@@ -14,11 +14,7 @@ export const CommentList = ({comments}) => {
 		<div className="w-100">
 			{
 				comments.map(item => (
-<<<<<<< HEAD
-					<div key={item.id} style={{backgroundColor: '#F8F8F8'}}>
-=======
 					<div>
->>>>>>> 044065e79951400a69812d6dfa299aa29ae8559c
 						<p style={{margin: '10px'}}>{item.author}</p>
 						<p style={{margin: '10px'}}>{item.content}</p>
 						<p style={{margin: '10px'}}>{calculateDate(item.date)}</p>
