c25c90ce636706100c7eb92bd37c6b5e16f24cac
kkjh9909 <kkjh990957@gmail.com>
2023-06-12 22:24:42 +0900

    메인 페이지 로고 변경

--- my-app/src/components/Nav.js
+++ my-app/src/components/Nav.js
@@ -14,12 +14,13 @@ import {
   faWonSign,
   faChartColumn,
   faTable,
-  faSignIn
+  faSignIn, faUserGroup, faBook, faThumbsUp, faBullhorn
 } from '@fortawesome/free-solid-svg-icons';
 import {Link, useNavigate} from "react-router-dom";
 import {Context} from "../store/Context";
 import jwt_decode from "jwt-decode";
 import {BsFillSunFill, BsFillMoonFill} from 'react-icons/bs';
+import {faCamera} from "@fortawesome/free-solid-svg-icons/faCamera";
 
 export const Nav = ({isDarkMode, toggleDarkMode}) => {
 
@@ -41,7 +42,12 @@ export const Nav = ({isDarkMode, toggleDarkMode}) => {
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
           </button>
-          <Link to='/' className="navbar-brand">자취생을 위한</Link>
+          {
+            !localStorage.getItem("mode") || localStorage.getItem("mode") === 'false' ? (
+            <Link to='/' className="navbar-brand" style={{padding: '0'}}><img src="/images/apple-light.png" alt="logo"/></Link> )
+                : (
+            <Link to='/' className="navbar-brand" style={{padding: '0'}}><img src="/images/apple-dark.png" alt="logo"/></Link> )
+          }
         </div>
         {/* <!-- /.navbar-header --> */}
 
@@ -170,85 +176,19 @@ export const Nav = ({isDarkMode, toggleDarkMode}) => {
                     <a href="#!"><FontAwesomeIcon icon={faChartColumn} /> 월별 사용량</a>
                   </li>
                   <li>
-                    <Link to='/karrot'><FontAwesomeIcon icon={faTable} /> 중고 거래</Link>
+                    <Link to='/karrot'><FontAwesomeIcon icon={faBook} /> 중고 거래</Link>
                   </li>
                   <li>
-                    <Link to='/group-buying'><FontAwesomeIcon icon={faTable} /> 공동 구매</Link>
+                    <Link to='/group-buying'><FontAwesomeIcon icon={faUserGroup} /> 공동 구매</Link>
                   </li>
                   <li>
-                    <Link to='/product-review'><FontAwesomeIcon icon={faTable} /> 물품 리뷰</Link>
+                    <Link to='/product-review'><FontAwesomeIcon icon={faCamera} /> 물품 리뷰</Link>
                   </li>
                   <li>
-                    <Link to='/government'><FontAwesomeIcon icon={faTable} /> 정부 정책</Link>
+                    <Link to='/government'><FontAwesomeIcon icon={faBullhorn} /> 정부 정책</Link>
                   </li>
                   <li>
-                    <Link to='/life-hack'><FontAwesomeIcon icon={faTable} /> 생활 꿀팁</Link>
-                  </li>
-                  <li>
-                    <a href="#!"><FontAwesomeIcon icon={faGear} /> 기타 드롭다운 메뉴 <span className="fa arrow"></span></a>
-                    <ul className="nav nav-second-level">
-                      <li>
-                        <a href="/panel-weels">Panels and Wells</a>
-                      </li>
-                      <li>
-                        <a href="/buttons">Buttons</a>
-                      </li>
-                      <li>
-                        <a href="/notifications">Notifications</a>
-                      </li>
-                      <li>
-                        <a href="/typography">Typography</a>
-                      </li>
-                      <li>
-                        <a href="/icons"> Icons</a>
-                      </li>
-                      <li>
-                        <a href="/grid">Grid</a>
-                      </li>
-                    </ul>
-                    {/* <!-- /.nav-second-level --> */}
-                  </li>
-                  <li>
-                    <a href="#!"><i className="fa fa-sitemap fa-fw"></i> 드롭다운 1<span className="fa arrow"></span></a>
-                    <ul className="nav nav-second-level">
-                      <li>
-                        <a href="#!">Secont Level Item</a>
-                      </li>
-                      <li>
-                        <a href="#!">Second Level Item</a>
-                      </li>
-                      <li>
-                        <a href="#!">드롭다운 2 <span className="fa arrow"></span></a>
-                        <ul className="nav nav-third-level">
-                          <li>
-                            <a href="#!">Third Level Item</a>
-                          </li>
-                          <li>
-                            <a href="#!">Third Level Item</a>
-                          </li>
-                          <li>
-                            <a href="#!">Third Level Item</a>
-                          </li>
-                          <li>
-                            <a href="#!">Third Level Item</a>
-                          </li>
-                        </ul>
-                        {/* <!-- /.nav-third-level --> */}
-                      </li>
-                    </ul>
-                    {/* <!-- /.nav-second-level --> */}
-                  </li>
-                  <li>
-                    <a href="#!"><i className="fa fa-files-o fa-fw"></i> Sample Pages<span className="fa arrow"></span></a>
-                    <ul className="nav nav-second-level">
-                      <li>
-                        <a href="/blank">Blank Page</a>
-                      </li>
-                      <li>
-                        <a href="/login">Login Page</a>
-                      </li>
-                    </ul>
-                    {/* <!-- /.nav-second-level --> */}
+                    <Link to='/life-hack'><FontAwesomeIcon icon={faThumbsUp} /> 생활 꿀팁</Link>
                   </li>
                 </ul>
               </div>
